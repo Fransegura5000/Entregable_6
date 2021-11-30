9436b6cc00fb1e0246a628aa4fec30d90997f724
Fransegura5000 <fseguraladat@gmail.com>
2021-11-30 15:16:04 +0100

    30/11/21

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,15 +6,19 @@ public class SpawnManager : MonoBehaviour
 {
     private float startDelay = 1f;
     private float repeatRate = 1.5f;
-    private Vector3 randomPos;
+  
     private float spawnPos;
-    // private Vector3 spawnPosL;
     private float randomHeight;
-    private float limY = 13f;
-    private PlayerController playerControllerScript;
-    public GameObject[] obstaclePrefabs;
 
-    // Start is called before the first frame update
+    private Vector3 randomPos;
+
+    private float limY = 13f;
+
+    public GameObject[] obstaclePrefabs;
+    private PlayerController playerControllerScript;
+ 
+
+    
     void Start()
     {
         // Repetimos la función SpawnObstacle cada 1.5 segundos y empieza en 1 segundo
@@ -22,15 +26,11 @@ public class SpawnManager : MonoBehaviour
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     public void SpawnObstacle()
     {
-        // Estamos accediendo a las variables del script PlayerController, en concreto, a la variable GameOver
+        // con esta variable hacemos que aparezcan los objetos en una posición aleatoria tanto en el eje Y como en el lado derecho o izquierdo 
+        
         if (!playerControllerScript.gameOver)
         {
             randomHeight = Random.Range(-limY, limY);
@@ -50,29 +50,8 @@ public class SpawnManager : MonoBehaviour
 
            
             
-            // spawnPosR = new Vector3(-15, randomHeight, 0);
-
-            // obstaclePrefabs.Length = indica la longitud máxima de prefabs que he puesto en SpawnManager
-            
-            
-            // Instantiate(obstaclePrefabs[randomIndex], spawnPosR, obstaclePrefabs[randomIndex].transform.rotation);
+          
             
         }
     }
 }
-
-
-/*
-n = Random.Range(0,2);
-if (n == 0)
-{
-    n = -1
-}
-
-
-spawnPos = Random.Range(0,2);
-if (spawnPos == 0)
-{
-    spawnPos = -1
-}
-*/
