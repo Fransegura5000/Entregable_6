7f0e279b6b14dd457dbb46dedd39198895941572
Fransegura5000 <fseguraladat@gmail.com>
2021-11-30 15:17:46 +0100

    31/11/21

--- Assets/Scripts/MoveLateral.cs
+++ Assets/Scripts/MoveLateral.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class MoveLateral : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 7f;
     private PlayerController playerControllerScript;
 
-    // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Estamos accediendo a las variables del script PlayerController, en concreto, a la variable GameOver
+        // con este if accedemos a la variable game over para ver si el juego ha de seguir en movimiento o pararse.
         if (!playerControllerScript.gameOver)
         {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
